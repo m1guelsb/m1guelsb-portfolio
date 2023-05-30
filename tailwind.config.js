8e816502953537421d66721b7aaf28e4b3ccbb6e
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-05-29 21:02:55 -0300

    Initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      "bg-gradient-1": "rgba(135, 0, 255, 0.26)",
+      "bg-gradient-2": "rgba(135, 0, 255, 0)",
+      "bg-gradient-3": "rgba(0, 138, 238, 0.33)",
+      "background-1": "#ffffff",
+      primary: "#6A67FF",
+      text1: "#595974",
     },
   },
   plugins: [],
-}
+};
